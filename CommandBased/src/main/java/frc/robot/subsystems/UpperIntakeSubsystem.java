53662ff2e9457e5d7b52f2775e853855f9a471bd
6744Leo <149526582+6744Leo@users.noreply.github.com>
2023-12-07 16:05:06 -0600

    commit

--- CommandBased/src/main/java/frc/robot/subsystems/UpperIntakeSubsystem.java
+++ CommandBased/src/main/java/frc/robot/subsystems/UpperIntakeSubsystem.java
@@ -3,16 +3,15 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 
 
 public class UpperIntakeSubsystem extends SubsystemBase {
 
 
-    
+
 
     
   private final WPI_VictorSPX leftGateMotor = new WPI_VictorSPX(8);
@@ -27,4 +26,14 @@ public class UpperIntakeSubsystem extends SubsystemBase {
     }
     
 
+   @Override
+  public void periodic() {
+    
+  }
+
+  public void setMotors(double intakeSpeed){
+    upperIntakeMotor.set(intakeSpeed);
+
+
   
+  }
